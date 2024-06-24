4cc309e4caeab6181fbf952038bc72c192409c6c
Thiago E. Santos <thi747@gmail.com>
2024-06-24 15:39:43 -0300

    Melhoria na montagem da Url de usage

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function obterParametroURL(nomeParam) {
 }
 
 function preencherInstrucoes() {
-  const uri = window.location.href;
+  const uri = window.location.href.split("?")[0];
   const empty = "".padEnd(TOTAL_UFS, 0);
   document.getElementById("usage").innerHTML += ` <em>${uri}</em>?q=${empty}`;
 }
