18e342345899cd64931ce70d4daa8e2e635369eb
Thiago E. Santos <2992801+thi747@users.noreply.github.com>
2025-05-22 09:58:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ const TOTAL_UFS = ESTADOS.length;
 document.getElementById("btnrandom").addEventListener("click", randomizar);
 
 // String de 27 dígitos (1/uf)
-const QUERY_STRING = validaQueryString(obterParametroURL("q") || "430435133060063604340005030");
+const QUERY_STRING = validaQueryString(obterParametroURL("q") || "430435133060063604140005030");
 preencherInstrucoes();
 preencherTabela();
 colorirEstados();
